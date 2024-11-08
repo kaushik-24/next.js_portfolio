31883633d59541a11656ffe2e74f56de681ce53b
kaushik <helworld6@gmail.com>
2024-11-08 13:47:45 +0545

    errrrorrr

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import MagicButton from './ui/MagicButton'
 import { FaLocationArrow } from 'react-icons/fa'
 import { socialMedia } from '@/data'
+import Image from 'next/image'
 
 const Footer = () => {
   return (
@@ -34,8 +35,9 @@ const Footer = () => {
             <div key={profile.id} className='w-10 h-10 cursor-pointer
             flex justify-center items-center backdrop-filter backdrop-blur-lg saturate-180
             bg-opacity-75 bg-black-200 rounded-lg border border-black-300'>
-              <img src={profile.img} alt={profile.id}
-              width='20' height='20'/>
+              <Image src={profile.img}
+              alt="icon"
+              width={20} height={20}/>
             </div>
           ))}
         </div>
