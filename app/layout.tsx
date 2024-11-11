12dae99539fdbc978204a73ec5f3d292d79eac69
kaushik <helworld6@gmail.com>
2024-11-11 12:54:00 +0545

    adding analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,6 +30,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <Analytics />
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
