5008663c65eada9c85b3acb0aba0d75b375749d2
kaushik <helworld6@gmail.com>
2024-11-11 12:39:21 +0545

    fix deployment

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,10 @@
 import Approach from "@/components/Approach";
-//import Clients from "@/components/Clients";
-//import Experience from "@/components/Experience";
 import Footer from "@/components/Footer";
 import Grid from "@/components/Grid";
 import Hero from "@/components/Hero";
 import RecentProjects from "@/components/RecentProjects";
 import { FloatingNav } from "@/components/ui/FloatingNav";
 import { navItems } from "@/data";
-
-
 
 export default function Home() {
   return (
@@ -24,8 +20,6 @@ export default function Home() {
         <Hero />
         <Grid />
         <RecentProjects />
-        {/*<Clients />
-        <Experience />*/}
         <Approach />
         <Footer />
       </div>
